f0dc1bb92dc9c9d73cfaa567803ba677913b159e
atran0802 <59665746+atran0802@users.noreply.github.com>
2020-03-04 13:05:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',function(req, res) {  
+  res.render('index',pageInfo.index);
+});
+
+app.get('/about',function(req, res) {  
+  res.render('about',pageInfo.about);
+});
+
+app.get('/services',function(req, res) {  
+  res.render('services',pageInfo.services);
+});
+
+app.get('/contact',function(req, res) {  
+  res.render('contact',pageInfo.contact);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
