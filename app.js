bc8b683080adb9a6aae29d1818ddbd5a1105a0cb
atran0802 <59665746+atran0802@users.noreply.github.com>
2020-03-04 22:01:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const path = require('path');
 const express = require('express');
+const pageInfo = require('./pageInfo');
+const ejs = require('ejs');
 
+/* Runs express */
 const app = express();
 
+/* Runs ejs */
+app.set('view engine', 'ejs');
+
+/* Get endpoint handlers to render */
 app.get('/',function(req, res) {  
   res.render('index',pageInfo.index);
 });
@@ -19,15 +26,18 @@ app.get('/contact',function(req, res) {
   res.render('contact',pageInfo.contact);
 });
 
+/* Serving static files in express */
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Returns 404 error */
 app.use(function(req, res, next) {
   res.status(404);
   res.send('404: File Not Found');
 });
 
+/* Localhost: 3000 */
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
   console.log(`Listening on port ${PORT}`);
 });
