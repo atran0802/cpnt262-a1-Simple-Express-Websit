eeb763b23d4aa25ff1dab825e9cb8668ef6cce70
atran0802 <59665746+atran0802@users.noreply.github.com>
2020-03-04 22:02:03 -0700

    Create pageInfo.js

---
+++ pageInfo.js
@@ -0,0 +1,20 @@
+const pageInfo = {
+  index: {
+    title: 'Home Page',
+    description: 'Home Page section content'
+  },
+  about: {
+    title: 'About',
+    description: 'About section content',
+  },
+  services: {
+    title: 'Services',
+    description: 'Services section content',
+  },
+  contact: {
+    title: 'Contact',
+    description: 'Contact section content',
+  }
+}
+
+module.exports = pageInfo;
